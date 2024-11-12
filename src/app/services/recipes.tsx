baa6394f9d8be79f8303c4aeff3d85827d697360
noa <abmagori@gmail.com>
2024-11-12 18:39:29 +0200

    vercel-4

--- src/app/services/recipes.tsx
+++ src/app/services/recipes.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { IRecipe } from "@/app/types/recipe";
 
 export const getRecipes = async (): Promise<IRecipe[]> => {
-  return (await axios.get("http://localhost:3000")).data;
+  return (await axios.get("https://recipes-three-gamma.vercel.app/pages/recipes")).data;
 };
 
 // export const getUserById = async (id: string) => {
