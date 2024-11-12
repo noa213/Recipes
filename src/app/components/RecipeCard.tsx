5fb6cc9e86d38d8d4e013930761b73082ad3bb9a
noa <abmagori@gmail.com>
2024-11-12 13:12:31 +0200

    recipe server CRUD

--- src/app/components/RecipeCard.tsx
+++ src/app/components/RecipeCard.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { IRecipe } from '../types/recipe';
-// import Image from 'next/image';
+import Image from 'next/image';
 
 
 const RecipeCard = ({ recipe }:{recipe:IRecipe}) => {
     return (
         <div className="recipe-card">
-          {/* <Image src={recipe.image} width={100} height={100} alt={recipe.name} /> */}
+          <Image src={recipe.image} width={100} height={100} alt={recipe.name} />
           <h3>{recipe.name}</h3>
           <p><strong>קטגוריה:</strong> {recipe.category}</p>
           {/* <p>{recipe.description}</p> */}
